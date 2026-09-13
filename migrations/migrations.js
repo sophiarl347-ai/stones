e7319a8ebf1f85526b39dbe3852b727317f8d499
sophiarl347-ai <sophiarl347@gmail.com>
2026-09-13 11:46:49 -0700

    update migrations/migrations.js

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -1,9 +1,13 @@
 import journal from "./meta/_journal.json";
 import m0000 from "./0000_init.sql";
+import m0001 from "./0001_add_options.sql";
+import m0002 from "./0002_real_item_fields.sql";
 
 export default {
   journal,
   migrations: {
-    m0000
+    m0000,
+    m0001,
+    m0002
   }
 };
